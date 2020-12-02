7227541f4258ce527a4aa667dcb333d05bb855d0
Adam Graham <adam.zigurous@gmail.com>
2020-12-02 00:54:10 -0600

    Add gallery slideshow functionality

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import AppMenuBar from './AppMenuBar';
 import '../styles/gallery.css';
 
-const Gallery = ({ className, slides = [] }) => (
-  <React.Fragment>
-    <div className={classNames('gallery', className)}>
-      {slides.map((slide) => (
-        <div className="gallery__slide" key={slide.id}>
-          <div className="gallery__image-wrapper">
-            <img alt="" className="gallery__image" src={slide.image}></img>
-          </div>
-          <div className="gallery__text-wrapper">
-            <div className="h4">{slide.title}</div>
-            <p>{slide.description}</p>
-            <a className="font-weight-semibold" href={slide.link}>
-              {slide.linkText || 'More Details'}
-            </a>
-          </div>
+const Gallery = ({ className, slides = [] }) => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  return (
+    <React.Fragment>
+      <div className={classNames('gallery', className)}>
+        <button
+          className="gallery__button left"
+          disabled={slideIndex <= 0}
+          onClick={() => {
+            setSlideIndex(Math.max(slideIndex - 1, 0));
+          }}
+        >
+          {'<'}
+        </button>
+        <div className="gallery__slides">
+          {slides.map((slide, index) => (
+            <div
+              className={classNames('slide', {
+                inactive: slideIndex !== index,
+              })}
+              key={slide.id}
+            >
+              <div className="slide__image-wrapper">
+                <img alt="" className="slide__image" src={slide.image}></img>
+              </div>
+              <div className="slide__text-container">
+                <div className="h4">{slide.title}</div>
+                <p>{slide.description}</p>
+                <a className="font-weight-semibold" href={slide.link}>
+                  {slide.linkText || 'More Details'}
+                </a>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-    <AppMenuBar />
-  </React.Fragment>
-);
+        <button
+          className="gallery__button right"
+          disabled={slideIndex >= slides.length - 1}
+          onClick={() => {
+            setSlideIndex(Math.min(slideIndex + 1, slides.length - 1));
+          }}
+        >
+          {'>'}
+        </button>
+      </div>
+      <AppMenuBar />
+    </React.Fragment>
+  );
+};
 
 Gallery.propTypes = {
   className: PropTypes.string,
