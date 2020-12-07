581521d739c0249ba73d26d73927eeed4e998b54
Adam Graham <adam.zigurous@gmail.com>
2020-12-06 22:47:15 -0600

    Add <Elegy> project page

---
+++ src/pages/projects/Elegy.js
@@ -0,0 +1,54 @@
+import { Button, ImageGallery } from '@zigurous/react-components';
+import React from 'react';
+import { Project } from '../../components';
+import elegyScreenshot01 from '../../images/screenshots/elegy-screenshot-01.png';
+import elegyScreenshot02 from '../../images/screenshots/elegy-screenshot-02.png';
+
+const Elegy = () => (
+  <Project>
+    <section>
+      <h1>Elegy</h1>
+    </section>
+    <section>
+      <p>
+        Elegy is a deep game covering the five stages of grief as a parent deals
+        with the loss of their child. A parent attends the funeral of their
+        child and tries to rewind time to stop the child&apos;s death from
+        occurring. The player engages in different scenes to figure out how they
+        can stop the events from happening, but ultimately they must accept the
+        death of their child. Elegy was developed with a team of five people in
+        less than two weeks at DePaul University.
+      </p>
+    </section>
+    <section>
+      <p className="body-sm">
+        <em>ROLE —</em> Lead Programmer, Designer
+        <br />
+        <em>TECH —</em> Adobe Flash, ActionScript 3.0
+        <br />
+        <em>DATE —</em> March 2015
+      </p>
+    </section>
+    <section>
+      <Button
+        external
+        link="https://drive.google.com/open?id=1RHmvqyU1B5gXo0hb18n7ahSxnKQeSTra"
+        size="small"
+      >
+        Download
+      </Button>
+    </section>
+    <section className="height-1em" />
+    <section>
+      <p className="body-lg">
+        <b>Media</b>
+        <ImageGallery
+          columns={1}
+          images={[elegyScreenshot01, elegyScreenshot02]}
+        />
+      </p>
+    </section>
+  </Project>
+);
+
+export default Elegy;
