aef9a3adf68012f1ff522a53e4f687947ac38a33
Adam Graham <adam.zigurous@gmail.com>
2020-12-06 22:10:33 -0600

    Add <BossRush> project page

---
+++ src/pages/projects/BossRush.js
@@ -0,0 +1,51 @@
+import { Button, ImageGallery } from '@zigurous/react-components';
+import React from 'react';
+import { Project } from '../../components';
+import bossRushScreenshot01 from '../../images/screenshots/boss-rush-screenshot-01.png';
+import bossRushScreenshot02 from '../../images/screenshots/boss-rush-screenshot-02.png';
+
+const BossRush = () => (
+  <Project>
+    <section>
+      <h1>Boss Rush</h1>
+    </section>
+    <section>
+      <p>
+        Boss Rush is a top-down, twin-stick shooter consisting of nothing but
+        boss fights. Developed with a team of seven people for a course at
+        DePaul University, the objective was to create a slice of a complete
+        game, so it consists of one action-packed boss fight – the Spider Tank.
+      </p>
+    </section>
+    <section>
+      <p className="body-sm">
+        <em>ROLE —</em> Gameplay &amp; Systems Programmer
+        <br />
+        <em>TECH —</em> Unity, C#
+        <br />
+        <em>DATE —</em> November 2014
+      </p>
+    </section>
+    <section>
+      <Button
+        external
+        link="https://github.com/ModSquadWorkshop/BossRush/releases"
+        size="small"
+      >
+        Download
+      </Button>
+    </section>
+    <section className="height-1em" />
+    <section>
+      <p className="body-lg">
+        <b>Media</b>
+        <ImageGallery
+          columns={1}
+          images={[bossRushScreenshot01, bossRushScreenshot02]}
+        />
+      </p>
+    </section>
+  </Project>
+);
+
+export default BossRush;
