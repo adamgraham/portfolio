275673421a3f87f8deaf8d7fb8f3ff0f34e1c187
Adam Graham <adam.zigurous@gmail.com>
2020-12-06 20:39:44 -0600

    Add <TheRise> project page

---
+++ src/pages/projects/TheRise.js
@@ -0,0 +1,61 @@
+import { Button, ImageGallery } from '@zigurous/react-components';
+import React from 'react';
+import { Project } from '../../components';
+import theRiseScreenshot01 from '../../images/screenshots/the-rise-screenshot-01.png';
+import theRiseScreenshot02 from '../../images/screenshots/the-rise-screenshot-02.png';
+import theRiseScreenshot03 from '../../images/screenshots/the-rise-screenshot-03.png';
+import theRiseScreenshot04 from '../../images/screenshots/the-rise-screenshot-04.png';
+import theRiseScreenshot05 from '../../images/screenshots/the-rise-screenshot-05.png';
+import theRiseThumbnail from '../../images/thumbnails/the-rise-thumbnail.png';
+
+const TheRise = () => (
+  <Project>
+    <section>
+      <h1>The Rise</h1>
+    </section>
+    <section>
+      <p>
+        The Rise is a &quot;doors escape&quot; puzzle game with a total of 30
+        unique levels. Each is an individual puzzle designed to make you think
+        hard. Put your logical skills to the test, rise to the top, and conquer
+        all 30 levels.
+      </p>
+    </section>
+    <section>
+      <p className="body-sm">
+        <em>ROLE —</em> Lead Programmer, Producer, Designer
+        <br />
+        <em>TECH —</em> Adobe Flash, ActionScript 3.0
+        <br />
+        <em>DATE —</em> July 2013
+      </p>
+    </section>
+    <section>
+      <Button
+        external
+        link="http://www.newgrounds.com/portal/view/622585"
+        size="small"
+      >
+        Play Game
+      </Button>
+    </section>
+    <section className="height-1em" />
+    <section>
+      <p className="body-xl">
+        <b>Media</b>
+      </p>
+      <img alt="" width="100%" src={theRiseThumbnail} />
+      <ImageGallery
+        images={[
+          theRiseScreenshot01,
+          theRiseScreenshot02,
+          theRiseScreenshot03,
+          theRiseScreenshot04,
+          theRiseScreenshot05,
+        ]}
+      />
+    </section>
+  </Project>
+);
+
+export default TheRise;
