6135885bffbfeb85ed12bd23502bfd73b1d7a7a6
Adam Graham <adam.zigurous@gmail.com>
2020-12-08 01:38:58 -0600

    Add <SquishemWebsite> project page

---
+++ src/pages/projects/SquishemWebsite.js
@@ -0,0 +1,43 @@
+import { Link } from '@zigurous/react-components';
+import React from 'react';
+import { Project } from '../../components';
+import screenshot from '../../images/screenshots/squishem-screenshot-spread.png';
+
+const Squishem = () => (
+  <Project>
+    <section>
+      <h1>Squish-em!</h1>
+    </section>
+    <section>
+      <p>
+        Squish-em! is an arcade game with ever so squishy moles just asking for
+        a tap on the head! It is a simple whack-a-mole re-creation designed for
+        highscore-play. Squish your way to the top of the leaderboards, earn
+        achievements along the way, and brag to your friends once you beat their
+        best score! Do you have what it takes to become the best squisher
+        around?
+      </p>
+      <p>
+        The Squish-em! website is a one-page, artistic cover designed to promote
+        the game and direct users to download the mobile app.
+      </p>
+    </section>
+    <section>
+      <p>
+        <Link external href="http://www.squishem.com/">
+          <b>Website</b>
+        </Link>
+      </p>
+      <Link
+        className="image-link"
+        external
+        href="http://www.squishem.com/"
+        undecorated
+      >
+        <img alt="Squish-em! Website" src={screenshot} />
+      </Link>
+    </section>
+  </Project>
+);
+
+export default Squishem;
