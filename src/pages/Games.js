f25e3a2f5fd7b17e7664535b27609150ff566cb6
Adam Graham <adam@zigurous.com>
2020-12-09 23:08:24 -0600

    Shorten Elegy gallery description

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -124,7 +124,7 @@ const Games = () => (
         id: 'elegy',
         title: 'Elegy',
         description:
-          "Elegy is a deep game covering the five stages of grief as a parent deals with the loss of their child. A parent attends the funeral of their child and tries to rewind time to stop the child's death from occurring. The player engages in different scenes to figure out how they can stop the events from happening, but ultimately they must accept the death of their child.",
+          "Elegy is a deep game covering the five stages of grief as a parent deals with the loss of their child. A parent attends the funeral of their child and tries to rewind time to stop the child's death from occurring, but ultimately they must accept the death of their child.",
         link: '/games/elegy',
         altText: 'Elegy Painting',
         image: paintingElegy,
