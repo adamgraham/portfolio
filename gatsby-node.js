8b9ef218eebaf6acd13eae2e6b0112963e311b65
Adam Graham <adam@zigurous.com>
2021-04-16 03:53:08 -0500

    Add graphql scheme type definitions

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,8 +11,102 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   });
 };
 
-// exports.createSchemaCustomization = ({ actions }) => {
-//   const { createTypes } = actions;
-//   const typeDefs = ``;
-//   createTypes(typeDefs);
-// };
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+  const typeDefs = `
+    type ProjectDetail implements Node {
+      key: String!
+      value: String!
+    }
+    type ProjectButton implements Node {
+      name: String!
+      link: String!
+      icon: String
+    }
+    type ProjectSection implements Node {
+      title: String
+      link: String
+      mainImage: File @fileByRelativePath
+      mainVideo: String
+      paragraphs: [String]
+      gallery: [File] @fileByRelativePath
+      videos: [String]
+    }
+    type ArtJson implements Node {
+      id: String!
+      category: String!
+      title: String!
+      date: String
+      description: String
+      description_short: String
+      description_long: [String]
+      image: ImageSharp! @fileByRelativePath
+      imageAltText: String
+      imageBorder: String
+      details: [ProjectDetail]
+      buttons: [ProjectButton]
+      sections: [ProjectSection]
+    }
+    type GamesJson implements Node {
+      id: String!
+      category: String!
+      title: String!
+      date: String
+      description: String
+      description_short: String
+      description_long: [String]
+      image: ImageSharp! @fileByRelativePath
+      imageAltText: String
+      imageBorder: String
+      details: [ProjectDetail]
+      buttons: [ProjectButton]
+      sections: [ProjectSection]
+    }
+    type PresentationsJson implements Node {
+      id: String!
+      category: String!
+      title: String!
+      date: String
+      description: String
+      description_short: String
+      description_long: [String]
+      image: ImageSharp! @fileByRelativePath
+      imageAltText: String
+      imageBorder: String
+      details: [ProjectDetail]
+      buttons: [ProjectButton]
+      sections: [ProjectSection]
+    }
+    type TechJson implements Node {
+      id: String!
+      category: String!
+      title: String!
+      date: String
+      description: String
+      description_short: String
+      description_long: [String]
+      image: ImageSharp! @fileByRelativePath
+      imageAltText: String
+      imageBorder: String
+      details: [ProjectDetail]
+      buttons: [ProjectButton]
+      sections: [ProjectSection]
+    }
+    type WebsitesJson implements Node {
+      id: String!
+      category: String!
+      title: String!
+      date: String
+      description: String
+      description_short: String
+      description_long: [String]
+      image: ImageSharp! @fileByRelativePath
+      imageAltText: String
+      imageBorder: String
+      details: [ProjectDetail]
+      buttons: [ProjectButton]
+      sections: [ProjectSection]
+    }
+  `;
+  createTypes(typeDefs);
+};
