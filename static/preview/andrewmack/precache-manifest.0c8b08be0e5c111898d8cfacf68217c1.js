00c03b63b08ce90634e0b59a0136e138376e34f0
Adam Graham <adam@zigurous.com>
2021-09-05 22:41:07 -0500

    Add andrewmack website preview

---
+++ static/preview/andrewmack/precache-manifest.0c8b08be0e5c111898d8cfacf68217c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6d5ae54a1e4155d991019e632530b65",
+    "url": "/static/media/character-unmasked.a6d5ae54.png"
+  },
+  {
+    "revision": "371fbc9e2044febda15dba0be837d36e",
+    "url": "/static/media/character-masked.371fbc9e.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f8d0a200eb2546e8eb82",
+    "url": "/static/js/main.f8d0a200.chunk.js"
+  },
+  {
+    "revision": "3f0b22993c54ddcf39f8",
+    "url": "/static/js/1.3f0b2299.chunk.js"
+  },
+  {
+    "revision": "f8d0a200eb2546e8eb82",
+    "url": "/static/css/main.ca03012c.chunk.css"
+  },
+  {
+    "revision": "28bdc980c2bc166fd21876e9a548e40c",
+    "url": "/index.html"
+  }
+];
---
+++ static/preview/andrewmack/precache-manifest.8580af4302463551874c2e67d47bd85c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6d5ae54a1e4155d991019e632530b65",
+    "url": "/static/media/character-unmasked.a6d5ae54.png"
+  },
+  {
+    "revision": "371fbc9e2044febda15dba0be837d36e",
+    "url": "/static/media/character-masked.371fbc9e.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ac86791aae23eec49ba",
+    "url": "/static/js/main.1ac86791.chunk.js"
+  },
+  {
+    "revision": "3f0b22993c54ddcf39f8",
+    "url": "/static/js/1.3f0b2299.chunk.js"
+  },
+  {
+    "revision": "1ac86791aae23eec49ba",
+    "url": "/static/css/main.b0737510.chunk.css"
+  },
+  {
+    "revision": "440d4cd2d7a9d294e765fcb998ea135e",
+    "url": "/index.html"
+  }
+];
