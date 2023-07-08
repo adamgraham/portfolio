3e15352cf898247c0de1bdc6d99f3bb8b3cacca7
Adam Graham <adam@zigurous.com>
2023-07-08 17:04:35 -0500

    Update cover page description

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,14 @@ const Home = () => {
         <b className="h5 padding-left-sm">Hello, my name is</b>
         <h1 className="margin-top-sm">Adam Graham</h1>
         <p className="text-foreground-secondary font-lg margin-bottom-1em">
-          I am a game developer and software engineer based in Chicago, IL
-          fueled by a passion for art and design. I founded an indie game studio
-          called{' '}
+          Adam is a professional software engineer with over 10 years experience
+          in the industry. He founded the indie game development studio{' '}
           <Link external to="https://zigurous.com/">
             <b>Zigurous</b>
           </Link>{' '}
-          where I make Unity assets and tutorials to help developers make games
-          easier.
+          in 2021 and is currently focused on creating Unity assets and
+          tutorials for other developers. His work is inspired by a passion for
+          art, design, and engineering.
         </p>
         <Link ElementType={GatsbyLink} to="/games" unstyled>
           <Button size={Button.size.medium}>View Gallery</Button>
