30e0f8a7e62461831ae70229b4ed26218b089d3f
Adam Graham <adam@zigurous.com>
2025-06-10 18:39:53 -0500

    Update logo and favicon

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,5 @@
 import './src/styles/global.css';
 
-const updateFavicon = isDark => {
-  const favicon = document.querySelector('link[rel*="icon"]');
-  if (favicon) {
-    favicon.href = isDark ? '/favicon-dark.png' : '/favicon-light.png';
-  }
-};
-
-export const onRouteUpdate = () => {
-  const darkMode = window.matchMedia('(prefers-color-scheme:dark)');
-  updateFavicon(darkMode.matches);
-  darkMode.addEventListener('change', e => {
-    updateFavicon(e.matches);
-  });
-};
-
 export const onInitialClientRender = () => {
   const storage = localStorage.getItem('theme');
   const theme = storage && JSON.parse(storage);
